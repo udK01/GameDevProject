94522a1375e9a9685d305a356ac82bd924b095b5
udK01 <dominik.muranyi@gmail.com>
2023-02-19 08:27:13 +0000

    Obstacle placement corrected, obstacle movement added.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,17 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
+        if (Input.GetKeyDown(KeyCode.W)) {
             Move(Vector3.up);
         }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
+        if (Input.GetKeyDown(KeyCode.A)) {
             Move(Vector3.left);
         }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
+        if (Input.GetKeyDown(KeyCode.D)) {
             Move(Vector3.right);
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+            Move(Vector3.down);
         }
     }
 
