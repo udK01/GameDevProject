c0c4b6a3b6a79f010991f6ba075386ebc145552a
udK01 <dominik.muranyi@gmail.com>
2023-02-19 09:34:02 +0000

    Added colliders

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,6 +21,35 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move(Vector3 direction)
     {
-        transform.position += direction;
+        Vector3 destination = transform.position + direction;
+
+        Collider2D barrier = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Barrier"));
+        Collider2D platform = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Platform"));
+        Collider2D obstacle = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Obstacle"));
+
+        if (barrier != null)
+        {
+            return;
+        }
+        if (platform != null)
+        {
+            transform.SetParent(platform.transform);
+        } else
+        {
+            transform.SetParent(null);
+        }
+        if (obstacle != null)
+        {
+            transform.position += direction;
+            Death();
+        } else
+        {
+            transform.position += direction;
+        }
+    }
+
+    public void Death()
+    {
+        enabled = false;
     }
 }
