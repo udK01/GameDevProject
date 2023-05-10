faf37cfaa8467cbfecd780ac2cead56386315cae
udK01 <dominik.muranyi@gmail.com>
2023-05-10 15:10:10 +0100

    Added rewind, moved scripts.

---
+++ Assets/Scripts/TimeRewind.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeRewind : MonoBehaviour
+{
+
+    private bool isRewinding = true;
+
+    List<Vector3> positions = new List<Vector3>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && Player.Instance.gameObject.activeSelf)
+        {
+            StartRewind();
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            StopRewind();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isRewinding)
+        {
+            Rewind();
+        } else
+        {
+            Record();
+        }
+    }
+
+    private void Rewind()
+    {
+        if (positions.Count > 0)
+        {
+            transform.position = positions[0];
+            positions.RemoveAt(0);
+        } else
+        {
+            StopRewind();
+        }
+    }
+
+    private void Record()
+    {
+        if (positions.Count > Mathf.Round(3f / Time.fixedDeltaTime))
+        {
+            positions.RemoveAt(positions.Count - 1);
+        }
+        positions.Insert(0, transform.position);
+    }
+
+    public void StartRewind()
+    {
+        isRewinding = true;
+    }
+
+    public void StopRewind()
+    {
+        isRewinding = false;
+    }
+}
